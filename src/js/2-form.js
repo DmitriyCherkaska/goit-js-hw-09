b6b7b0b91669e235f6cfbaf2a385a8622e02bbf2
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-04-15 16:04:19 +0300

    2-js

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('.feedback-form');
+const inputEmail = form.elements.email;
+const textarea = form.elements.message;
+const localKeyStorage = 'feedback-form-state';
+
+
+form.addEventListener('input', function () {
+  const formData = {
+    email: inputEmail.value.trim(),
+    message: textarea.value.trim(),
+  };
+
+  localStorage.setItem(localKeyStorage, JSON.stringify(formData));
+});
+
+
+function updateFormContent() {
+  const formUpdate = JSON.parse(localStorage.getItem(localKeyStorage)) || {};
+  inputEmail.value = formUpdate.email ?? '';
+  textarea.value = formUpdate.message ?? '';
+}
+
+updateFormContent();
+
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formSubmit = JSON.parse(localStorage.getItem(localKeyStorage)) || {};
+
+  if (formSubmit.email && formSubmit.message) {
+    console.log(formSubmit);
+
+    localStorage.removeItem(localKeyStorage);
+    form.reset();
+  }
